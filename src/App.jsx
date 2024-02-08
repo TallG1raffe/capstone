a7a21f901affdc7baa841755123dd6fdf3a31178
Ben Cat <ben@cat.com>
2024-02-08 11:48:45 -0500

    Added inputs to main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App() {
+
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({ ...values, [name]: value }))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputs);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={handleSubmit}>
+        <h2>Name</h2>
+        <input
+          type="text"
+          name="name"
+          value={inputs.name || ""}
+          onChange={handleChange}
+        />
+        <h2>Department</h2>
+        <input
+          type="text"
+          name="department"
+          value={inputs.department || ""}
+          onChange={handleChange}
+        />
+        <h2>Feedback</h2>
+        <textarea
+          type="text"
+          name="content"
+          value={inputs.content || ""}
+          onChange={handleChange}
+          rows={4}
+          cols={40} />
+        <br />
+        <input type="submit" />
+      </form>
     </>
   )
 }
-
-export default App
