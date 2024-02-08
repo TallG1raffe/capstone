1310d66ab5d8133a4d5425c679838bd6775345ca
Ben Cat <ben@cat.com>
2024-02-08 10:44:09 -0500

    added responses api

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import { MongoClient } from 'mongodb';
+
 
 const app = express();
 
@@ -6,6 +8,19 @@ app.use(express.json());
 
 app.get("/api/test", async (req, res) => {
   res.json("Test");
+});
+
+app.get("/api/responses", async (req, res) => {
+  try {
+    const client = await MongoClient.connect('mongodb://localhost:27017');
+    const db = client.db('Feedback');
+    const collection = db.collection('responses');
+    const responses = await collection.find().toArray();
+    client.close();
+    res.json(responses);
+  } catch (error) {
+    res.status(500).json({error: error});
+  }
 });
 
 const port = 3000;
