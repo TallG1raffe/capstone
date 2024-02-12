d36d85348f105c6a6db5f9e149776328692ae96e
Ben Cat <ben@cat.com>
2024-02-12 09:54:38 -0500

    Added login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { Link, Route, Routes } from 'react-router-dom'
+import { Login } from './Login';
 import { Form } from './Form';
 import { Response } from './Response';
 
@@ -8,19 +9,24 @@ export function App() {
   const departments = ['Customer Service', 'Engineering', 'Finance', 'Human Resources', 'IT', 'Marketing', 'Operations', 'Research and Development', 'Sales']
 
   return (
-    <div className='container'>
-      <nav>
-      {departments.map(department =>
-      <>
-        <Link to={`responses/${department}`}>
-          <button key={department}>{department}</button>
-        </Link>
-      </>)}
-      </nav>
-      <Routes>
-        <Route path="/" element={<Form />}></Route>
-        <Route path="/responses/:id" element={<Response />}></Route>
-      </Routes>
-    </div>
+    <>
+      <div className='container'>
+        <Routes>
+          <Route path="/" element={<Login />}></Route>
+          <Route path="/home" element={<>
+            <nav>
+              {departments.map(department =>
+                <>
+                  <Link to={`responses/${department}`}>
+                    <button key={department}>{department}</button>
+                  </Link>
+                </>)}
+            </nav>
+            <Form /></>} >
+          </Route>
+          <Route path="/responses/:id" element={<Response />}></Route>
+        </Routes>
+      </div >
+    </>
   )
 }
---
+++ src/Login.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'
+
+
+export function Login({ onLogin }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form>
+        <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <Link to={`/home`}><button type="submit">Login</button></Link>
+      </form>
+    </div>
+  );
+};
